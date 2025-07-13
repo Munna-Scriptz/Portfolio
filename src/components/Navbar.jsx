4b6e6b4bea4faf8fbd3248acf310b169c9bfa045
Munna-Scripts <munnascriptz@gmail.com>
2025-07-13 18:14:54 +0600

    Navbar 50% Complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import Logo from "../assets/images/Logo.png"
+import { Link } from 'react-router'
+import { LuDot } from "react-icons/lu";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+        <nav className='py-5'>
+            <div className="container">
+                <div id='NavRow' className='flex items-center justify-between'>
+                    {/* ----Logo---- */}
+                    <div className='w-[60px]'>
+                        <img src={Logo} alt="Logo" />
+                    </div>
+                    {/* ----NavLinks---- */}
+                    <div>
+                        <ul className='font-soldier flex items-center gap-3 text-2xl text-Primary'>
+                            <li><Link to={'/'} className='flex items-center NavLinks'>HOME<LuDot/></Link></li>
+                            <li><Link to={'/'} className='flex items-center NavLinks'>ABOUT<LuDot/></Link></li>
+                            <li><Link to={'/'} className='flex items-center NavLinks'>PROJECTS<LuDot/></Link></li>
+                            <li><Link to={'/'} className='flex items-center NavLinks'>CONTACT<LuDot/></Link></li>
+                        </ul>
+                    </div>
+                    {/* ----Button---- */}
+                    <div>
+                        <Link className='ContactButton font-poppins font-medium text-base text-Primary'>CONTACT</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </>
   )
 }

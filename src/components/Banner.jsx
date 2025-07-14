e2137e6aa2eb142d26324d629d33db953b7920d2
Munna-Scripts <munnascriptz@gmail.com>
2025-07-14 15:34:05 +0600

    Type Writter Effect Added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
   return (
     <>
-       <section id="Banner">
+       <section id="Banner" className='pt-22'>
             <div className="container">
-                <h1>Hello im munna</h1>
+                <div className="bannerText">
+                    <h1 className='font-poppins text-Primary text-[60px]'>Hi, i'm Munna</h1>
+                    {/* -----animated Text------- */}
+                    <div className='mt-2'>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Im A Frontend Developer',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Im A Backend Developer',
+                                1000,
+                                'Im A Fullstack Developer',
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            style={{ fontSize: '36px', color:' #161616', fontFamily: 'Poppins, sans-serif' , display: 'inline-block' }}
+                            repeat={Infinity}
+                        />
+                    </div>
+                    
+                </div>
             </div>
         </section> 
     </>

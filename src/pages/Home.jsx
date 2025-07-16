89ad4ba455347d3546efad21ca8575eb93f8f895
Munna-Scripts <munnascriptz@gmail.com>
2025-07-16 21:08:40 +0600

    Counter Component created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import Counter from '../components/Counter'
 
 export const Home = () => {
   return (
     <>
       <Banner/>
+      <Counter/>
     </>
   )
 }

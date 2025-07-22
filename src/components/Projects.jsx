aea6a80fda484020c03d0872913215528c0f2859
Munna-Scripts <munnascriptz@gmail.com>
2025-07-22 18:16:29 +0600

    Project Tools Used Added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
   return (
     <>
     <section id="Projects">
-      <div className="container">
+      <div>
           <div className="header mb-20">
             <div className='flex items-center justify-center font-poppins text-Primary font-medium text-2xl'><ScrollFloat animationDuration={1} ease='back.inOut(2)' scrollStart='center bottom+=50%' scrollEnd='bottom bottom-=40%' stagger={0.03}>MY PROJECTS</ScrollFloat></div>
             <div className='flex items-center justify-center font-soldier text-Primary font-medium text-4xl tracking-[5px] mt-5'><ScrollFloat animationDuration={1} ease='back.inOut(2)' scrollStart='center bottom+=70%' scrollEnd='bottom bottom-=40%' stagger={0.03}>EXPLORE MY PROJECTS</ScrollFloat></div>
--- src/components/ProjectsShow.jsx
+++ src/components/ProjectsShow.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import ProjectImg1 from '../assets/images/Project1.jpg'
+import { GrTechnology } from "react-icons/gr";
+import ReactIcon from '../assets/images/react.svg'
+import FigmaIcon from '../assets/images/FigmaIcon.svg'
+import TailwindCss from '../assets/images/TailwindCssIcon.svg'
+import JsIcon from '../assets/images/JsIcon.jpg'
+import HtmlIcon from '../assets/images/HtmlIcon.svg'
+import CssIcon from '../assets/images/CssIcon.svg'
 
 
 const ProjectsShow = () => {
   return (
 
     <>
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-25 px-10'>
             <div className='w-[640px]'>
                 <img className='rounded-xl' src={ProjectImg1} alt="Project Image" />
             </div>
             {/* -------Project Details  */}
-            <div>
-                <h2>NEXTON E-COMMERCE</h2>
+            <div className='w-[650px]'>
+                <h2 className='font-soldier text-[44px] text-Primary font-medium mb-4'>NEXTON E-COMMERCE</h2>
+                <h2 className='font-poppins text-[19px] text-Primary font-normal mb-7'>Nexton is a sleek and modern e-commerce platform designed to offer a smooth shopping experience with dynamic product listings, variant selection, and responsive design. Built with React and crafted for performance and style</h2>
+                
+                <div className='flex items-center gap-4'>
+                    <p className='font-poppins text-[19px] text-second font-medium flex items-center gap-2'><GrTechnology /> Tools Used :</p>
+                    <div className='w-[30px] flex items-center gap-2'>
+                        <img src={ReactIcon} alt="Icon" />
+                        <img src={TailwindCss} alt="Icon" />
+                        <img src={JsIcon} alt="Icon" />
+                        <img src={FigmaIcon} alt="Icon" />
+                        <img src={HtmlIcon} alt="Icon" />
+                        <img src={CssIcon} alt="Icon" />
+                    </div>
+                </div>
             </div>
         </div>
     </>

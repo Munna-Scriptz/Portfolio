a167a490b586c6dd107ac54dbb9ffaa4aecce97f
Munna-Scripts <munnascriptz@gmail.com>
2025-07-22 21:12:36 +0600

    Projects Component complete

--- src/components/ProjectsShow.jsx
+++ src/components/ProjectsShow.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+// ---------Project images 
 import ProjectImg1 from '../assets/images/Project1.jpg'
 import ProjectImg2 from '../assets/images/Project2.png'
-
+import ProjectImg3 from '../assets/images/Project3.png'
+import ProjectImg4 from '../assets/images/Project4.png'
+// -------icons 
 import { GrTechnology } from "react-icons/gr";
+// -------Technologies svg 
 import ReactIcon from '../assets/images/react.svg'
 import FigmaIcon from '../assets/images/FigmaIcon.svg'
 import TailwindCss from '../assets/images/TailwindCssIcon.svg'
 import JsIcon from '../assets/images/JsIcon.jpg'
 import HtmlIcon from '../assets/images/HtmlIcon.svg'
 import CssIcon from '../assets/images/CssIcon.svg'
+import Bootstrap from '../assets/images/BootStrapIcon.svg'
+// -----other Import 
 import Magnet from './common/Magnet'
 import { Link } from 'react-router';
 
@@ -21,34 +27,26 @@ const ProjectsShow = () => {
             'ProjectImage' : ProjectImg1,
             'ProjectName' : 'nexton e - commerce',
             'Description' : 'Nexton is a sleek and modern e-commerce platform designed to offer a smooth shopping experience with dynamic product listings, variant selection, and responsive design. Built with React and crafted for performance and style',
-            'technologies1' : ReactIcon,
-            'technologies2' : TailwindCss,
-            'technologies3' : JsIcon,
-            'technologies4' : FigmaIcon,
-            'technologies5' : HtmlIcon,
-            'technologies6' : CssIcon,
+            'technologies' : [ReactIcon , TailwindCss , JsIcon ,FigmaIcon , HtmlIcon , CssIcon]
         },
         {
             'ProjectImage' : ProjectImg2,
             'ProjectName' : 'future scooter',
             'Description' : 'A modern and vibrant landing page for an E-Scooter brand, highlighting features, community engagement, and sleek UI design. Built for performance and visual appeal, perfect for urban mobility startups',
-            'technologies1' : ReactIcon,
-            'technologies2' : TailwindCss,
-            'technologies3' : JsIcon,
-            'technologies4' : FigmaIcon,
-            'technologies5' : HtmlIcon,
-            'technologies6' : CssIcon,
+            'technologies' : [JsIcon ,FigmaIcon ,Bootstrap, HtmlIcon , CssIcon],
             'Direction': 'flex-row-reverse'
         },
         {
-            'ProjectName' : 'nexton e - commerce',
-            'Description' : 'Nexton is a sleek and modern e-commerce platform designed to offer a smooth shopping experience with dynamic product listings, variant selection, and responsive design. Built with React and crafted for performance and style',
-            'technologies' : [ReactIcon , TailwindCss , JsIcon ,FigmaIcon , HtmlIcon , CssIcon]
+            'ProjectImage' : ProjectImg3,
+            'ProjectName' : 'Nescafe foods',
+            'Description' : 'A stylish and modern restaurant landing page for Nescafe-themed food services. Showcases menus, deals, and cozy vibes with a clean UI, perfect for cafes and food brands',
+            'technologies' : [JsIcon ,FigmaIcon, Bootstrap , HtmlIcon , CssIcon]
         },
         {
-            'ProjectName' : 'nexton e - commerce',
-            'Description' : 'Nexton is a sleek and modern e-commerce platform designed to offer a smooth shopping experience with dynamic product listings, variant selection, and responsive design. Built with React and crafted for performance and style',
-            'technologies' : [ReactIcon , TailwindCss , JsIcon ,FigmaIcon , HtmlIcon , CssIcon],
+            'ProjectImage' : ProjectImg4,
+            'ProjectName' : 'furniture landing page',
+            'Description' : 'NexA minimal and elegant single-page website for a furniture brand, highlighting premium products with smooth UI, large visuals, and a modern layout designed for conversion',
+            'technologies' : [JsIcon ,FigmaIcon ,Bootstrap, HtmlIcon , CssIcon],
             'Direction': 'flex-row-reverse'
         }
     ]
@@ -71,17 +69,16 @@ const ProjectsShow = () => {
                                 <div className='flex items-center gap-4'>
                                     <p className='font-poppins text-[19px] text-second font-medium flex items-center gap-2'><GrTechnology /> Tools Used :</p>
                                     <div className='w-[30px] flex items-center gap-2'>
-                                        <img src={items.technologies1} alt="Icon" />
-                                        <img src={items.technologies2} alt="Icon" />
-                                        <img src={items.technologies3} alt="Icon" />
-                                        <img src={items.technologies4} alt="Icon" />
-                                        <img src={items.technologies5} alt="Icon" />
-                                        <img src={items.technologies6} alt="Icon" />
+                                        {
+                                            items.technologies.map((img , i)=>(
+                                                <img src={img} key={i}/>
+                                            ))
+                                        }
                                     </div>
                                 </div>
                                 <div className='mt-7 flex items-center gap-4'>
                                     <Magnet padding={30} disabled={false} magnetStrength={5}>
-                                        <Link className='ContactButton font-poppins font-medium text-base text-Primary'>VIEW FULL PROJECT</Link>
+                                        <Link className='ContactButton font-poppins font-medium text-base text-Primary'>VIEW ALL PROJECT</Link>
                                     </Magnet>
                                     <Magnet padding={10} disabled={false} magnetStrength={5}>
                                         <Link className='DownloadCv font-poppins font-medium text-base text-Primary'>

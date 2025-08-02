1292c9d9084b85ed8fdd9e6013a93d5cf51775f5
Munna-Scripts <munnascriptz@gmail.com>
2025-08-02 21:14:32 +0600

    About me responsived

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ export const Home = () => {
       <Counter/>
       <LetsWorkTogether/>
       <div className="relative">
-            <div className="lg:sticky top-0 h-[1200px] lg:py-[120px] py-[20px] bg-[#E4E2DB]">
+            <div className="lg:sticky top-0 lg:h-[1200px] lg:py-[120px] py-[110px] bg-[#E4E2DB]">
               <AboutMe/>
             </div>
             <div className="lg:sticky top-0 h-[900px] bg-[#E4E2DB] border-t border-Primary">

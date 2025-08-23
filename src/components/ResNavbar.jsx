1c26da792ea2a47bf05f6efe77e901966f04cf53
Munna-Scripts <munnascriptz@gmail.com>
2025-08-23 13:58:07 +0600

    ResNavbar Complete

--- src/components/ResNavbar.jsx
+++ src/components/ResNavbar.jsx
@@ -6,59 +6,55 @@ import { LuShoppingBag } from "react-icons/lu"
 import { IoHomeOutline } from 'react-icons/io5';
 import { RiUserLine } from 'react-icons/ri';
 import { TbArrowsJoin } from 'react-icons/tb';
+import { GrContact, GrContactInfo, GrProjects } from 'react-icons/gr';
 
 export const ResNavbar = () => {
 
-    const [Value , SetValue] = useState('true')
+  const [Value , SetValue] = useState('true')
 
   return (
-    <>
-    
+  <>
     <nav id='Navbar' className='py-[25px] lg:hidden relative'>
       <div id='NavRow' className='flex container items-center justify-between overflow-hidden'>
         {/* ------Logo------ */}
-        <Link to={'/'} className='w-[40px] h-[40px]'><img src={ResLogo} alt="Logo" /></Link>
-
+        <Link data-aos="fade-down" data-aos-duration="1500" data-aos-delay="800" data-aos-easing="ease-in-sine" to={'/'} className='w-[40px] h-[40px] z-50'><img src={ResLogo} alt="Logo" /></Link>
         {/* ------Nav Menu------ */}
-    
-        <button className='relative w-[32px] h-[28px] cursor-pointer' onClick={()=>SetValue(!Value)}>
+        <div onClick={()=>SetValue(!Value)} className='font-soldier select-none cursor-pointer flex duration-[.3s] items-center gap-3 text-xl text-Primary z-50' data-aos="fade-down" data-aos-duration="1500" data-aos-delay="1200" data-aos-easing="ease-in-sine">
+          <p>{Value? 'MENU' : 'CLOSE'}</p>
+          <button className='relative w-[32px] h-[19px] cursor-pointer'>
             <span className={`w-[20px] h-[2px] rounded-[8px] bg-Primary absolute right-[10px] duration-[.3s] ${Value? 'rotate-[0deg] top-[3px]' : 'rotate-[45deg] top-[7px]'}`} ></span>
             <span className={`w-[20px] h-[2px] rounded-[8px] bg-Primary absolute top-[9px] right-[10px] duration-[.3s] ${Value? 'opacity-100' : 'opacity-0'}`}></span>
             <span className={`w-[20px] h-[2px] rounded-[8px] bg-Primary absolute right-[10px] duration-[.3s] ${Value? 'rotate-[0deg] top-[14px]' : 'rotate-[-45deg] top-[7px]' }`}></span>
-        </button>
-        {/* ------Button Menu------ */}
-        <div className={`fixed top-0 left-0 w-full h-screen transition-opacity duration-300 ${Value ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'} z-[50]`}>
-            {/*------------Overlay----------- */}
-            <div onClick={() => SetValue(!Value)} className={`w-full h-screen fixed top-0 left-0 bg-[#00000070] transition-opacity duration-[.3s] ${Value ? 'opacity-0' : 'opacity-100'}`}
-            ></div>
-            <div className={`h-[90%] w-full fixed bottom-0 bg-[#E5E3DC] p-5 duration-500 shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden transform ${Value ? 'translate-y-full' : 'translate-y-0'} z-[60]`}>
-                
-              {/* -------------Header------------- */}
-              <div className='flex items-center justify-between absolute top-0 right-0 w-full p-5'>
-                  <Link to={'/'} className='w-[40px]'><img src={ResLogo} alt="Logo" /></Link>
-                  <button className='relative w-[32px] h-[28px] cursor-pointer right-0' onClick={() => SetValue(!Value)}>
-                      <span className={`w-[20px] h-[2px] bg-Primary absolute right-[10px] transition-all duration-[.3s] ${Value ? 'rotate-0 top-[5px]' : 'rotate-45 top-[7px]'}`}></span>
-                      <span className={`w-[20px] h-[2px] bg-Primary absolute top-[10px] right-[10px] transition-all duration-[.3s] ${Value ? 'opacity-100' : 'opacity-0'}`}></span>
-                      <span className={`w-[20px] h-[2px] bg-Primary absolute right-[10px] transition-all duration-[.3s] ${Value ? 'rotate-0 top-[15px]' : '-rotate-45 top-[7px]'}`}></span>
-                  </button>
-              </div>
-              
-              {/* ------------Nav Link-------- */}
-              <ul className='flex flex-col items-start gap-5 text-xl mt-25'>
-                  <li><Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/'}><IoHomeOutline/> Home</Link></li>
-                  <span className='w-full h-[1px] bg-gray-400'></span>
-                  <li><Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/about'}><MdOutlineGroup/> About</Link></li>
-                  <span className='w-full h-[1px] bg-gray-400'></span>
-                  <li><Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/projects'}><MdOutlineGroup/> Projects</Link></li>
-                  <span className='w-full h-[1px] bg-gray-400'></span>
-                  <li><Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/contact'}><MdOutlineGroup/> Contact</Link></li>
-              </ul>
-            </div>
+          </button>
         </div>
-      </div>
+        {/* ------Button Menu------ */}
+        <div className={`fixed top-0 left-0 w-full h-screen transition-opacity duration-[.8s] ${Value ? 'opacity-0 pointer-events-none' : 'opacity-100 pointer-events-auto'} z-[40]`}>
+          {/*------------Overlay----------- */}
+          <div onClick={() => SetValue(!Value)} className={`w-full h-screen fixed top-0 left-0 bg-[#00000070] transition-opacity duration-300 ${Value ? 'opacity-0' : 'opacity-100'}`}></div>
 
-    </nav>
+          {/* ------------Drawer----------- */}
+          <div className={`h-full w-full fixed bottom-0 bg-[#E5E3DC] p-5 duration-[.4s] shadow-[0_3px_10px_rgb(0,0,0,0.2)] overflow-hidden transform ${Value ? 'translate-y-full' : 'translate-y-0'} z-[60]`}>
+            {/* ------------Nav Links-------- */}
+            <ul className={`flex flex-col items-center gap-8 text-3xl font-soldier mt-32 transition-all duration-[.4s] ${Value ? 'opacity-0 -translate-y-10' : 'opacity-100 translate-y-0'}`}>
+              <li className={`transition-all duration-[.4s] delay-200 ${Value ? 'opacity-0 translate-y-10' : 'opacity-100 translate-y-0'}`}>
+                <Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/'}><IoHomeOutline/> Home</Link>
+              </li>
+              <li className={`transition-all duration-[.4s] delay-300 ${Value ? 'opacity-0 translate-y-10' : 'opacity-100 translate-y-0'}`}>
+                <Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/about'}><GrContactInfo /> About me</Link>
+              </li>
+              <li className={`transition-all duration-[.4s] delay-400 ${Value ? 'opacity-0 translate-y-10' : 'opacity-100 translate-y-0'}`}>
+                <Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/projects'}><GrProjects className='text-xl'/> Projects</Link>
+              </li>
+              <li className={`transition-all duration-[.4s] delay-500 ${Value ? 'opacity-0 translate-y-10' : 'opacity-100 translate-y-0'}`}>
+                <Link onClick={() => SetValue(!Value)} className='text-Primary font-manrope font-medium flex items-center gap-2' to={'/contact'}><GrContact className='text-[22px]' /> Contact</Link>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </div>
+
+  </nav>
     
-    </>
+  </>
   )
 }

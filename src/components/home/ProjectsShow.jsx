497cfae9ec5548c943c8ac26b82989492acccbbe
Munna-Scripts <munnascriptz@gmail.com>
2025-10-11 21:29:53 +0600

    Home projects updated

--- src/components/home/ProjectsShow.jsx
+++ src/components/home/ProjectsShow.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
 // ---------Project images 
-import ProjectImg1 from '../../assets/images/Project1.jpg'
+import nextonProject from '../../assets/images/Project1.jpg'
+import sansaProject from '../../assets/images/sansaProject.jpg'
 import HectoCommerce from '../../assets/images/HectoThumbnail.png'
-import ProjectImg2 from '../../assets/images/Project2.png'
-import ProjectImg3 from '../../assets/images/Project3.png'
+import noteProject from '../../assets/images/NoteAppThumb.png'
+import canvixProject from '../../assets/images/CanvixThumb.png'
 // -------icons 
 import { GrTechnology } from "react-icons/gr";
 // -------Technologies svg 
 import ReactIcon from '../../assets/images/react.svg'
+import NextJsIcon from '../../assets/images/NextJsIcon.svg'
 import FigmaIcon from '../../assets/images/FigmaIcon.svg'
 import TailwindCss from '../../assets/images/TailwindCssIcon.svg'
 import reduxIcon from '../../assets/images/ReduxIcon.svg'
@@ -34,40 +36,49 @@ const ProjectsShow = () => {
     const MyProjects = [
 
         {
-            'ProjectImage' : ProjectImg1,
+            'ProjectImage' : sansaProject,
+            'ProjectName' : 'Sansa e - commerce',
+            'Description' : 'Sansa is a full e-commerce website with 10+ paGES which provide users to order online and pick up product eastly, contain features like add to cart, search, product preview etc. Made with next js and fully responsive across all devices.',
+            'technologies' : [NextJsIcon , TailwindCss , JsIcon , reduxIcon ,FigmaIcon , HtmlIcon , CssIcon],
+            'Direction': 'lg:flex-row',
+            'AosDir' : 'fade-right',
+            'GithubRepo' : 'https://sansa-commerce.vercel.app/'
+        },
+        {
+            'ProjectImage' : nextonProject,
             'ProjectName' : 'nexton e - commerce',
             'Description' : 'Nexton is a sleek and modern e-commerce platform designed to offer a smooth shopping experience with dynamic product listings, variant selection, and responsive design. Built with React and crafted for performance and style',
             'technologies' : [ReactIcon , TailwindCss , JsIcon , reduxIcon ,FigmaIcon , HtmlIcon , CssIcon],
-            'Direction': 'lg:flex-row',
-            'AosDir' : 'fade-right',
-            'GithubRepo' : 'https://github.com/Munna-Scriptz/Nexton-E-Commerce.git'
+            'Direction': 'lg:flex-row-reverse',
+            'AosDir' : 'fade-left',
+            'GithubRepo' : 'https://nexton-e-commerce-chi.vercel.app/'
         },
         {
             'ProjectImage' : HectoCommerce,
             'ProjectName' : 'Hecto E-Commerce',
             'Description' : 'The Hecto E-Commerce project is a responsive 15-page website showcasing stylish furniture with a clean, user-friendly interface. Built with React.js, Tailwind CSS, Redux, and Firebase, it delivers a seamless shopping experience with smooth navigation, interactive components, and API integrations, highlighting advanced web development skills.',
             'technologies' : [ReactIcon , TailwindCss , JsIcon , reduxIcon , FigmaIcon , HtmlIcon , CssIcon],
-            'Direction': 'lg:flex-row-reverse',
-            'AosDir' : 'fade-left',
-            'GithubRepo' : 'https://github.com/Munna-Scriptz/Hecto-E-Commerce.git'
-        },
-        {
-            'ProjectImage' : ProjectImg2,
-            'ProjectName' : 'future scooter',
-            'Description' : 'A modern and vibrant landing page for an E-Scooter brand, highlighting features, community engagement, and sleek UI design. Built for performance and visual appeal, perfect for urban mobility startups',
-            'technologies' : [JsIcon ,FigmaIcon ,Bootstrap, HtmlIcon , CssIcon],
             'Direction': 'lg:flex-row',
             'AosDir' : 'fade-right',
-            'GithubRepo' : 'https://github.com/Munna-Scriptz/E-Scooter.git'
+            'GithubRepo' : 'https://hecto-e-commerce.vercel.app/'
         },
         {
-            'ProjectImage' : ProjectImg3,
-            'ProjectName' : 'Nescafe foods',
-            'Description' : 'A stylish and modern restaurant landing page for Nescafe-themed food services. Showcases menus, deals, and cozy vibes with a clean UI, perfect for cafes and food brands',
-            'technologies' : [JsIcon ,FigmaIcon, Bootstrap , HtmlIcon , CssIcon],
+            'ProjectImage' : noteProject,
+            'ProjectName' : 'Eclipse Note App',
+            'Description' : 'A responsive Note App built entirely with React JS, Tailwind CSS, and Firebase Realtime Database, featuring secure authentication, clean UI, and smooth note management. It includes create, edit, delete, and recover from bin options with real-time data sync.',
+            'technologies' : [JsIcon ,FigmaIcon ,Bootstrap, HtmlIcon , CssIcon],
             'Direction': 'lg:flex-row-reverse',
             'AosDir' : 'fade-left',
-            'GithubRepo' : 'https://github.com/Munna-Scriptz/Nescafe_web.git'
+            'GithubRepo' : 'https://note-app-pi-flame.vercel.app/'
+        },
+        {
+            'ProjectImage' : canvixProject,
+            'ProjectName' : 'Canvix Agency',
+            'Description' : 'Canvix Agency is a full multi-page website built with React JS and Tailwind CSS, designed for agencies to showcase services, portfolio, team, testimonials, blog, and contact. It features polished visuals,smooth transitions. Fully responsive across all devices, it highlights professionalism and strong content structure and usability.',
+            'technologies' : [JsIcon ,FigmaIcon, Bootstrap , HtmlIcon , CssIcon],
+            'Direction': 'lg:flex-row',
+            'AosDir' : 'fade-right',
+            'GithubRepo' : 'https://canvix-agency.vercel.app/'
         },
     ]
   return (
@@ -102,7 +113,7 @@ const ProjectsShow = () => {
                                     </Magnet>
                                     <Magnet padding={10} disabled={false} magnetStrength={5}>
                                         <a href={`${items.GithubRepo}`} target='_blank' className='DownloadCv font-poppins font-medium lg:text-base text-sm text-Primary hover-brown'>
-                                            <span>GITHUB REPO</span>
+                                            <span>LIVE LINK</span>
                                         </a>
                                     </Magnet>
                                 </div>
